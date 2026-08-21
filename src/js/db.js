63dff09a52c2cdfe15f37edcb9373414aee3e9b1
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-08-21 14:55:39 -0600

    Constant keys

--- src/js/db.js
+++ src/js/db.js
@@ -7,11 +7,21 @@ db.version(1).stores({
     options: 'key, value'
 });
 
+/**
+ * Constant keys for values available to store.
+ * Do not use single strings.
+ */
+export const Keys = {
+    PAGE_TO_LOAD: 'PAGE_TO_LOAD'
+}
+
+Object.freeze(Keys);
+
 export async function saveOption(key, value) {
     await db.options.put({ key, value });
 }
 
-export async function getOption(key) {
+export async function getOption(key, defaultValue) {
     const option = await db.options.get(key);
-    return option ? option.value : null;
+    return option ? option.value : defaultValue;
 }
