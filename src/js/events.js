b2f48a870a00e6f6a1e67551c5b3546b6d76fd64
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-09-07 12:56:09 -0600

    string events replaced with constants

--- src/js/events.js
+++ src/js/events.js
@@ -1,5 +1,15 @@
 export const LangEvent = {
-    LANG_UPDATED: 'LANG_UPDATED',
+    UPDATED: 'lang_updated'
 }
-
 Object.seal(LangEvent);
+
+export const CamEvent = {
+    RESTART: 'cam_restart'
+}
+Object.seal(CamEvent);
+
+export const ProductEvent ={
+    UPDATED: 'product_updated',
+    ADD: 'product_add'
+}
+Object.seal(ProductEvent);
