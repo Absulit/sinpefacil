8abada7108d1eb5a7e2aa730fea40aac88055af1
Sebastian Sanabria Diaz <sebastian@absulit.com>
2026-09-13 14:15:42 -0600

    updated tests

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-    await page.goto('http://localhost:5173/sinpefacil/');
+    await page.goto('http://localhost:5173/sinpefacil/', { waitUntil: 'domcontentloaded' });
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/SINPE Fácil/);
